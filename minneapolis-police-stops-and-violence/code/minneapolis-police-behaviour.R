25e3346a7036e42e87dcc1b7ec09d3990da0d681
weiyuet <weiyuet@gmail.com>
2022-08-29 23:41:43 +0800

    Updated .gitignore

--- minneapolis-police-stops-and-violence/code/minneapolis-police-behaviour.R
+++ minneapolis-police-stops-and-violence/code/minneapolis-police-behaviour.R
@@ -2,12 +2,14 @@
 library(tidyverse)
 library(scales)
 library(patchwork)
+library(lubridate)
 
 # Load data
 police_stops <- read_csv("minneapolis-police-stops-and-violence/data/police_stop_data.csv")
 police_use_of_force <- read_csv("minneapolis-police-stops-and-violence/data/police_use_of_force.csv")
 
 # Preliminary EDA
+# Force and violence categories
 p1 = police_use_of_force %>% 
   ggplot(aes(x = ForceType)) +
   geom_bar() +
@@ -41,3 +43,26 @@ p3 = police_stops %>%
 
 # Save png
 ggsave("minneapolis-police-stops-and-violence/figures/police-force-subcategories.png", width = 8, height = 9)
+
+# Time distributions of police violence
+# Within a day
+force_hour_count <- police_use_of_force %>% 
+  mutate(hour = hour(ResponseDate),
+         year = year(ResponseDate)) %>% 
+  filter(year != 1970) %>% 
+  group_by(hour) %>% 
+  summarise(count = n()) %>% 
+  mutate(percent = count / sum(count))
+
+p2 <- police_stops %>% 
+  mutate(hour = hour(responseDate)) %>% 
+  group_by(hour) %>% 
+  summarise(count = n()) %>% 
+  mutate(percent = count / sum(count)) %>% 
+  ggplot(aes(x = hour, y = percent)) +
+  geom_line(aes(colour = "Stop"), size = 1) +
+  geom_line(data = force_hour_count, aes(x = hour, y = percent, colour = "Force use"), size = 1) +
+  scale_x_continuous(breaks = seq(0, 23, 2)) +
+  theme_classic() +
+  theme(legend.position = "none") +
+  labs(x = "Hour of day", y = "Proportion of day")
