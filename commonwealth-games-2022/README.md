0574fb59b9dbd97359255624a06bd01289a39465
weiyuet <weiyuet@gmail.com>
2022-10-03 18:51:28 +0800

    Updated various files in kaggle

--- commonwealth-games-2022/README.md
+++ commonwealth-games-2022/README.md
@@ -1,3 +1,5 @@
+## commonwealth-games-2022
+
 ![](https://github.com/weiyuet/kaggle/blob/main/commonwealth-games-2022/figures/countries-most-athletes.png)
 
 ![](https://github.com/weiyuet/kaggle/blob/main/commonwealth-games-2022/figures/total-gold-country.png)
--- minneapolis-police-stops-and-violence/code/minneapolis-police-behaviour.R
+++ minneapolis-police-stops-and-violence/code/minneapolis-police-behaviour.R
@@ -1,51 +1,53 @@
-# Load libraries
+#Setup
 library(tidyverse)
 library(scales)
 library(patchwork)
 library(lubridate)
 
-# Load data
+#Load data
 police_stops <- read_csv("minneapolis-police-stops-and-violence/data/police_stop_data.csv")
 police_use_of_force <- read_csv("minneapolis-police-stops-and-violence/data/police_use_of_force.csv")
 
-# Preliminary EDA
-# Force and violence categories
-p1 = police_use_of_force %>% 
+#Prelim EDA
+#Force and violence categories
+p1 <- police_use_of_force %>%
   ggplot(aes(x = ForceType)) +
   geom_bar() +
   geom_text(aes(label = ..count..), stat = "count", vjust = -0.3) +
-  scale_y_continuous(limits = c(0, 30000),
+  scale_y_continuous(expand = c(0, 0),
+                     limits = c(0, 30000),
                      labels = label_number(big.mark = ",")) +
   theme_classic() +
   theme(axis.text.x = element_text(angle = 30, hjust = 1)) +
   labs(x = "", y = "")
 
-p2 = police_stops %>% 
+p2 <- police_stops %>% 
   ggplot(aes(personSearch)) +
   geom_bar() +
   geom_text(aes(label = ..count..), stat = "count", vjust = -0.3) +
-  scale_y_continuous(labels = label_number(big.mark = ",")) +
+  scale_y_continuous(expand = c(0, 0),
+                     labels = label_number(big.mark = ",")) +
   theme_classic() +
   labs(x = "Person Search", y = "")
 
-p3 = police_stops %>% 
+p3 <- police_stops %>% 
   ggplot(aes(vehicleSearch)) +
   geom_bar() +
   geom_text(aes(label = ..count..), stat = "count", vjust = -0.3) +
-  scale_y_continuous(labels = label_number(big.mark = ",")) +
+  scale_y_continuous(expand = c(0, 0),
+                     labels = label_number(big.mark = ",")) +
   theme_classic() +
   labs(x = "Vehicle Search", y = "")
 
-# Combine plots and annotate
+#Combine plots and annotate
 (p2 | p3) / 
   p1 + plot_annotation(title = "Subcategories of Police Stops and Violence in Minneapolis",
                        caption = "Source: Kaggle")
 
-# Save png
+#Save png
 ggsave("minneapolis-police-stops-and-violence/figures/police-force-subcategories.png", width = 8, height = 9)
 
-# Time distributions of police violence
-# Within a day
+#Time distributions of police violence within a day
 force_hour_count <- police_use_of_force %>% 
   mutate(hour = hour(ResponseDate),
          year = year(ResponseDate)) %>% 
@@ -54,7 +56,7 @@ force_hour_count <- police_use_of_force %>%
   summarise(count = n()) %>% 
   mutate(percent = count / sum(count))
 
-p2 <- police_stops %>% 
+p4 <- police_stops %>% 
   mutate(hour = hour(responseDate)) %>% 
   group_by(hour) %>% 
   summarise(count = n()) %>% 
